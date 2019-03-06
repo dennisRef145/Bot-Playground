6c4c5b00e5bd294ee70113f1591de1e6f787cf9e
Dennis Maseluk <dennis989@web.de>
2019-03-06 08:00:37 +0100

    Add clones

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,56 +4,90 @@ import java.util.Scanner;
 
 public class Calculator extends SuperCalculator {
 
-	/**
-	 * [smell] This variable is never used
-	 */
-	private String resultAsText;
-
-	/**
-	 * [smell] Modifiers are not in correct order
-	 */
 	public final static int MULTIPLIER = 10;
 
 	public static void main(String[] args) {
 		new Calculator().runProgram();
 	}
 
-	/**
-	 * [smell] This method misses an @Override annotation
-	 */
+	@Override
 	public double addition(double a, double b) {
-		return a + b;
+		int x1;
+		int x2;
+		x1 = 12;
+		x2 = 13;
+		int x3 = 14;
+		int x4 = 15;
+		int x5 = 16;
+		int x6 = 17;
+		int x7 = 18;
+		int x8 = 19;
+		x1 = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8;
+		return a + b + x1;
 	}
 
-	/**
-	 * [smell] This method has an unused parameter
-	 * 
-	 * @param a
-	 * @param b
-	 * @param c
-	 *            this one here
-	 * @return
-	 */
+	public double additionClone(double a, double b) {
+		int y1;
+		int y2;
+		y1 = 12;
+		y2 = 13;
+		int y3 = 14;
+		int y4 = 15;
+		int y5 = 16;
+		int y6 = 17;
+		int y7 = 18;
+		int y8 = 19;
+		y1 = y1 + y2 + y3 + y4 + y5 + y6 + y7 + y8;
+		return a + b + y1;
+	}
+
 	public double multiplication(double a, double b, double c) {
 		return a * b;
 	}
+
+	public double multiplication(double a, double b) { return multiplication(a, b, 42); }
 
 	public double subtraction(double a, double b) {
 		return a - b;
 	}
 
 	public double division(double a, double b) {
-		return a / b;
+		int c;
+		int d;
+		int e;
+		int k;
+		int j;
+		c = 12;
+		d = 13;
+		e = 14;
+		k = 15;
+		j = 16;
+		c = c + d + e + k + j;
+		return a / b + c;
 	}
 
-	/**
-	 * [smell] This method is too long
-	 */
+	public double divisionClone(double a, double b) {
+		int f;
+		int c;
+		int d;
+		int e;
+		int k;
+		int j;
+		c = 12;
+		d = 13;
+		e = 14;
+		// test comment
+		k = 15;
+		j = 16;
+		c = c + d + e + k + j; // test comment 2
+		return a / b + c;
+	}
+
 	public void runProgram() {
 		println("The calculator is ready to calculate!");
 
 		// get numbers
-		Scanner inp = new Scanner(System.in);
+		Scanner inp= new Scanner(System.in);
 		int num1;
 		int num2;
 		println("Enter first number:");
@@ -67,25 +101,68 @@ public class Calculator extends SuperCalculator {
 		choose = inp.nextInt();
 
 		// calculate and print result
-		switch (choose) {
-		case 1:
-			println("The result of the addition is:");
-			println(addition(num1, num2));
-			break;
-		case 2:
-			println("The result of the subtraction is:");
-			println(subtraction(num1, num2));
-			break;
-		case 3:
-			println("The result of the multiplication is:");
-			println(multiplication(num1, num2, 1));
-			break;
-		case 4:
-			println("The result of the division is:");
-			println(division(num1, num2));
-			break;
-		default:
-			println("Illegal Operation");
+		switch (choose){
+			case 1:
+				println("The result of the addition is:");
+				println(addition( num1,num2));
+				break;
+			case 2:
+				println("The result of the subtraction is:");
+				println(subtraction( num1,num2));
+				break;
+			case 3:
+				println("The result of the multiplication is:");
+				println(multiplication( num1,num2));
+				break;
+			case 4:
+				println("The result of the division is:");
+				println(division( num1,num2));
+				break;
+			default:
+				println("Illegal Operation");
+		}
+
+		inp.close();
+		println("Thank you for using this awesome calculator.");
+	}
+
+	public void runProgramCloneTest() {
+		println("The calculator is ready to calculate!");
+
+		// get numbers
+		Scanner inp= new Scanner(System.in);
+		int num1;
+		int num2;
+		println("Enter first number:");
+		num1 = inp.nextInt();
+		println("Enter second number:");
+		num2 = inp.nextInt();
+
+		// get operation
+		println("Enter your selection: 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division:");
+		int choose;
+		choose = inp.nextInt();
+
+		// calculate and print result
+		switch (choose){
+			case 1:
+				println("The result of the addition is:");
+				println(addition( num1,num2));
+				break;
+			case 2:
+				println("The result of the subtraction is:");
+				println(subtraction( num1,num2));
+				break;
+			case 3:
+				println("The result of the multiplication is:");
+				println(multiplication( num1,num2));
+				break;
+			case 4:
+				println("The result of the division is:");
+				println(division( num1,num2));
+				break;
+			default:
+				println("Illegal Operation");
 		}
 
 		inp.close();
@@ -93,7 +170,7 @@ public class Calculator extends SuperCalculator {
 	}
 
 	private void println(Object object) {
-		System.out.println(object); // NOSONAR
+		System.out.println(object); //NOSONAR
 	}
 
 }
