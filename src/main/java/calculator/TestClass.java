f1b5a35265b5e7e6c509d2a53211c3e11423e1b5
Dennis Maseluk <dennis989@web.de>
2019-03-15 18:44:55 +0100

    Change test class

--- src/main/java/calculator/TestClass.java
+++ src/main/java/calculator/TestClass.java
@@ -2,49 +2,58 @@ package calculator;
 
 public class TestClass {
     public void simpleMethod() {
-    	int c = 12;
-    	int d = 13;
-    	
-    	if (c + d > 20) {
-    		System.out.println("1");
-        	System.out.println("2");
-        	System.out.println("3");
-    	}
-    	
-    	int e = 10;
-    	int f = 3;
-    	
-    	if (e * f > 20) {
-    		System.out.println("4");
-        	System.out.println("5");
-        	System.out.println("6");
-    	}
-    	
-    	// test
-    	System.out.println("7");
-    	
-    	int y = 12;
-    	int x = 19;
-    	
-    	if (x - y < 20) {
-    		System.out.println("8");
-        	System.out.println("9");
-    	}
-    	
-    	int g = 10;
-    	int h = 2;
-    	
-    	if (g * h > 15) {
-    		System.out.println("10");
-    	}
+    	int a = 0;
+        int[] b = {2 , 3, 4, 6};
 
-        int a = 0;
-        int b = 1;
-
-        if (a == b) {
-            b = 2;
+        // this is a test
+        for (int c : b) {
+            a += c;
+            System.out.println(a);
+            if (a == 10) {
+                break;
+            }
         }
 
         System.out.println(b);
+
+        int ac = 0;
+        int bc = 1;
+
+        if (ac == bc) {
+            bc = 2;
+        }
+
+        System.out.println(bc);
+        
+        int ad = 0;
+        int bd = 1;
+
+        if (ad == bd) {
+            bd = 2;
+        } else {
+            ad = 2;
+        }
+
+        System.out.println(bd);
+        
+        int af = 0;
+        int bf = 1;
+
+        switch (af) {
+            case 0:
+                bf = af;
+                break;
+            case 1:
+                bf = 69;
+                break;
+            case 2:
+                bf = 42;
+                break;
+            default:
+                bf = 1000;
+                break;
+        }
+
+        System.out.println(bf);
     }
 }
