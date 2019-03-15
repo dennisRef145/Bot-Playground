1cafc0d6324e33cc72e15b4b7e139cebf987e049
Dennis Maseluk <dennis989@web.de>
2019-03-15 17:42:44 +0100

    Change test class

--- src/main/java/calculator/TestClass.java
+++ src/main/java/calculator/TestClass.java
@@ -2,25 +2,25 @@ package calculator;
 
 public class TestClass {
     public void simpleMethod() {
-    	int q;
-    	int w;
-    	int e;
-    	int r;
-    	int t;
-    	int z;
-    	int u;
-    	int i;
-    	int o;
-    	int p;
-    	int s;
-    	int d;
-    	int f;
-    	int g;
-    	int h;
-    	int j;
-    	int k;
-    	int l;
-    	int y;
+    	System.out.println("1");
+    	System.out.println("2");
+    	System.out.println("3");
+    	System.out.println("4");
+    	System.out.println("5");
+    	System.out.println("6");
+    	System.out.println("7");
+    	System.out.println("8");
+    	System.out.println("9");
+    	System.out.println("10");
+    	System.out.println("11");
+    	System.out.println("12");
+    	System.out.println("13");
+    	System.out.println("14");
+    	System.out.println("15");
+    	System.out.println("16");
+    	System.out.println("17");
+    	System.out.println("18");
+    	System.out.println("19");
         int a = 0;
         int b = 1;
 
