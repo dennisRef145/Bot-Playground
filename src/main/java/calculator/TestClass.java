48a84c6570b05dc4066f9b93cf158833cba81079
Dennis Maseluk <dennis989@web.de>
2019-03-15 16:57:58 +0100

    Add new class with long method

---
+++ src/main/java/calculator/TestClass.java
@@ -0,0 +1,33 @@
+package calculator;
+
+public class TestClass {
+    public void simpleMethod() {
+    	int q;
+    	int w;
+    	int e;
+    	int r;
+    	int t;
+    	int z;
+    	int u;
+    	int i;
+    	int o;
+    	int p;
+    	int s;
+    	int d;
+    	int f;
+    	int g;
+    	int h;
+    	int j;
+    	int k;
+    	int l;
+    	int y;
+        int a = 0;
+        int b = 1;
+
+        if (a == b) {
+            b = 2;
+        }
+
+        System.out.println(b);
+    }
+}
