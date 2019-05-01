34937ba2cfd5ecf2874bf5299bcd663395454d48
Dennis Maseluk <dennis989@web.de>
2019-05-01 15:51:15 +0200

    Change clone

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -67,71 +67,8 @@ public class Calculator extends SuperCalculator {
 		return a / b + c;
 	}
 
-	public double divisionClone(double a, double b) {
-		int atest = 13;
-		int f;
-		int c;
-		int d;
-		int e;
-		int k;
-		int j;
-		c = 12;
-		d = 13 + atest;
-		e = 14;
-		// test comment1
-
-		// test comment2
-		// test comment3
-		k = 15;
-		j = 16;
-		c = c + d + e + k + j; // test comment 2
-		return a / b + c;
-	}
 
 	public void runProgram() {
-		println("The calculator is ready to calculate!");
-
-		// get numbers
-		Scanner inp= new Scanner(System.in);
-		int num1;
-		int num2;
-		println("Enter first number:");
-		num1 = inp.nextInt();
-		println("Enter second number:");
-		num2 = inp.nextInt();
-
-		// get operation
-		println("Enter your selection: 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division:");
-		int choose;
-		choose = inp.nextInt();
-
-		// calculate and print result
-		switch (choose){
-			case 1:
-				println("The result of the addition is:");
-				println(addition( num1,num2));
-				break;
-			case 2:
-				println("The result of the subtraction is:");
-				println(subtraction( num1,num2));
-				break;
-			case 3:
-				println("The result of the multiplication is:");
-				println(multiplication( num1,num2));
-				break;
-			case 4:
-				println("The result of the division is:");
-				println(division( num1,num2));
-				break;
-			default:
-				println("Illegal Operation");
-		}
-
-		inp.close();
-		println("Thank you for using this awesome calculator.");
-	}
-
-	public void runProgramCloneTest() {
 		println("The calculator is ready to calculate!");
 
 		// get numbers
